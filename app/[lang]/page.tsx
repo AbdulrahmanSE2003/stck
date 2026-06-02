87ffe679df49f763c13b288fedee781d7ca4bdb9
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-06-02 15:41:10 +0300

    feat: minor style at home page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,4 +1,3 @@
-import { useTranslations } from "next-intl";
 import Header from "@/features/home/components/Header";
 import Hero from "@/features/home/components/Hero";
 import StatsCards from "@/features/home/components/StatsCards";
@@ -6,7 +5,9 @@ import Footer from "@/features/home/components/Footer";
 
 export default function LandingPage() {
   return (
-    <section className={`bg-black min-h-screen`}>
+    <section className={`bg-black min-h-screen relative overflow-hidden`}>
+      <div className="absolute top-0 left-0 -translate-x-1/2 w-250 h-100 bg-main-teal/65 rounded-full blur-[120px] pointer-events-none" />
+
       {/* Header */}
       <Header />
 
